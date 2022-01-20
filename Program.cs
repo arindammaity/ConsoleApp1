58ef1cbbc3cf0df4480fbf0cd9a8ef3fc05cecb3
ARINDAM <ARINDAM@DESKTOP-ER2D6R0>
2022-01-20 09:28:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to world of c sharp");
+        }
+    }
+}
